609c38496b3f0ce9d4ac04a40d7f38241266aa07
Volodymyr Stashchenko <stashchenko@ukr.net>
2016-05-30 13:36:15 +0300

    add basic postgres configuration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160518134310) do
+ActiveRecord::Schema.define(version: 20160530095403) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
