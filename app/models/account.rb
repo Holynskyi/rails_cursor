5bf36943dc9763b347892aac53f6fee1a2dd36a7
Volodymyr Stashchenko <stashchenko@ukr.net>
2016-06-14 17:26:12 +0300

    add mocking

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,18 @@
+require 'net/http'
+
 class Account < ActiveRecord::Base
   belongs_to :user
 
   validates :name, presence: true, length: {minimum: 8}
   validates :name,format: {with: /\A(?=.*[a-z])[a-z\d]+\Z/i}
   validates :age, presence: true, numericality: {greater_than_or_equal_to: 18, less_than_or_equal_to: 100}
+
+  def get_weather
+    url = URI.parse('http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22Lviv%2C%20Ukraine%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
+    req = Net::HTTP::Get.new(url.to_s)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    JSON.parse(res.body)['query']['results']['channel']['item']['condition']['text']
+  end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -6,6 +6,11 @@ describe Account do
   it { expect(account).to validate_presence_of(:name) }
   it { expect(account).to validate_presence_of(:name) }
   it { expect(account).to validate_presence_of(:age) }
+
+  it 'should get weather' do
+     allow(account).to receive(:get_weather) { 'Sunny' }
+     expect(account.get_weather).to eql('Sunny')
+  end
 end
 
 
